d3089e1d8e98f31e43958d9359b4e0ba7deb9bd6
Alex Laursen <ajlaursen@aol.com>
2021-02-13 19:21:35 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
-
 import "./App.css";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import Title from "./components/Title";
-import Body from "./components/Body"
-import DropDown from "./components/DropDown"
+import Body from "./components/Body";
+import DropDown from "./components/DropDown";
 import React, { useEffect, useState } from "react";
 
-
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     const hideMenu = () => {
-      if(window.innerWidth > 768 && isOpen){
-      setIsOpen(false)
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
       }
     };
-      window.addEventListener('resize', hideMenu)
+    window.addEventListener("resize", hideMenu);
 
-      return () => {
-       window.removeEventListener('resize', hideMenu)
-    }
-  }
-  )
-
+    return () => {
+      window.removeEventListener("resize", hideMenu);
+    };
+  });
 
   return (
-    
     <div className="flex flex-col h-screen">
-      <Nav toggle={toggle}/>
-      <DropDown isOpen={isOpen}  toggle={toggle}/>
+      <Nav toggle={toggle} />
+      <DropDown isOpen={isOpen} toggle={toggle} />
       <Body />
       <Footer />
-      </div>
-    
+    </div>
   );
 }
 
